fd11fd85d3f91c519635bf24f678e671fff7fe45
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-03-21 04:35:11 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = 2;
+
+const num3 = 9;
+const num4 = 5;
+const mod = 9 - 5;
+
+const max = 20;
+
+
